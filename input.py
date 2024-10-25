43628e5c159cc2f3b404f7360f50d5e5d134150d
zahraherani <marukochan097@gmail.com>
2024-10-25 19:04:36 +0700

    Add files via upload

---
+++ input.py
@@ -0,0 +1,7 @@
+nama = input ('masukan nama anda:')
+print('anyoengg,', nama, '..selamat kamu terpilih sebagai raja terakhir abad ini!')
+#input kalkulator
+print('kalkulator luas persegi panjang\n')
+panjang = input('masukan panjang')
+lebar = input('masukan lebar:')
+print('Luas =', int(panjang) * int (lebar))
